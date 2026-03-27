bb90bb0fb046298928540b01ea82c4edba3addcb
Jean Valencia <72704457+Jean-Dv@users.noreply.github.com>
2026-03-27 15:05:35 -0500

    DP-3 feature spring cloud (Gateway) (#1)
    * feat(gateway): implementacion de una puerta de enlace API centralizada con enrutamiento y validación de token Bearer base.
    
    * migrar infraestructura de Zuul a Spring Cloud Gateway
    
    Se actualiza el pom.xml para incluir spring-cloud-starter-gateway y spring-boot-starter-webflux.
    
    Se reemplaza la configuración de zuul.routes por spring.cloud.gateway.routes en application.yaml.
    
    Se implementa AuthFilter como un GlobalFilter reactivo para la validación de tokens.
    
    Se comenta temporalmente la URI balanceada (lb://) y se habilita una ruta de prueba estática para validación del gateway en entornos sin microservicios activos.
    
    * feat: adjust AuthFilter to bypass authentication only for paths starting with '/auth/' or exactly '/auth'
    
    ---------
    
    Co-authored-by: KimberlyFigueroa21 <kimfigueroa210004@gmail.com>

---
+++ gateway/api-gateway/src/main/java/uptc/edu/co/api_gateway/filters/AuthFilter.java
@@ -0,0 +1,41 @@
+package uptc.edu.co.api_gateway.filters;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import reactor.core.publisher.Mono;
+
+@Component
+public class AuthFilter implements GlobalFilter, Ordered {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+       
+        String path = exchange.getRequest().getPath().toString();
+
+       
+        if (path.startsWith("/auth/") || path.equals("/auth")) {
+            return chain.filter(exchange);
+        }
+
+        String authHeader = exchange.getRequest()
+                                    .getHeaders()
+                                    .getFirst("Authorization");
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return -1; 
+    }
+}
